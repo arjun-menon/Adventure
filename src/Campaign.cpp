e24f2ae8171e0d1a44fb14874f1b6edaa46cac37
Arjun G. Menon <contact@arjungmenon.com>
2013-04-06 21:03:47 +0000

    Minor

--- src/Campaign.cpp
+++ src/Campaign.cpp
@@ -21,25 +21,25 @@ class Campaign : public Entity
 public:
     Campaign() : m(Dim(1024, 600), 32) {
         a = new PlaceholderEntityAABB( Pt(10, 10) , Dim(200, 100) );
-        //b = new PlaceholderEntityAABB( Pt(220, 210) , Dim(100, 100) );
-        b = new DynamicPlaceholderEntityAABB( Pt(220, 210) , Dim(100, 100), 0.1f, 0.2f );
-        c = new DynamicPlaceholderEntityAABB( Pt(300, 170) , Dim(10, 10), 0.1f, 0.2f );
+        b = new PlaceholderEntityAABB( Pt(220, 210) , Dim(100, 100) );
+        //b = new DynamicPlaceholderEntityAABB( Pt(220, 210) , Dim(100, 100), 0.1f, 0.2f );
+        //c = new DynamicPlaceholderEntityAABB( Pt(300, 170) , Dim(10, 10), 0.1f, 0.2f );
         //b = new PlaceholderEntityAABB( Pt(220, 210) , Dim(5, 5) );
-        //c = new PlaceholderEntityAABB( Pt(225.1, 210) , Dim(3, 3) );
+        c = new PlaceholderEntityAABB( Pt(225.1, 210) , Dim(3, 3) );
 
         set<EntityAABB *> collidingEntities;
         m.place(a, collidingEntities);
         m.place(b, collidingEntities);
         m.place(c, collidingEntities);
 
-        dynamic_cast<DynamicEntityTrait&>(*c).velocity.x = -7.0f;
+        //dynamic_cast<DynamicEntityTrait&>(*c).velocity.x = -7.0f;
     }
 
     void step() {
         m.step();
 
         set<EntityAABB *> collidingEntities;
-        //m.entityMap.moveBy(b, Pt(-5, 0), collidingEntities);
+        m.entityMap.moveBy(b, Pt(5, 0), collidingEntities);
     }
 };
 
