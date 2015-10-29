87b5a9d7f2ec89bf5dcaa786b94b63229cebd885
Arjun G. Menon <contact@arjungmenon.com>
2015-10-29 19:09:59 -0400

    WIP: Switch to SDL 2.0

--- src/PlatformInterface.cpp
+++ src/PlatformInterface.cpp
@@ -10,84 +10,97 @@
 #include <chrono>
 #include <random>
 
-#include <SFML/Graphics.hpp>
+#include <SDL2/SDL.h>
 
 class SystemImpl : public System
 {
 public:
-    SystemImpl() : gameMain(nullptr), renderWindow(nullptr),
-        rng( std::chrono::system_clock::now().time_since_epoch().count() ) {
-    }
+    SystemImpl() : gameMain(nullptr), renderWindow(nullptr), renderContext(nullptr), isRunning(false),
+                   rng(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count())) { }
 
-    ~SystemImpl() {
-    }
+    ~SystemImpl() { }
 
     shared_ptr<Tex> loadTex(const string file) {
-        if( fileTexMap.find(file) == fileTexMap.end() ) {
-            // Load the texture
-            sf::Image image;
-            if( !image.loadFromFile(file) )
-                throw invalid_argument("Unable to open file: " + file);
-
-            sf::Texture* tex = new sf::Texture;
-            if( !tex->loadFromImage(image) )
-                throw runtime_error("Loading image from texture " + file + " failed.");
-
-            fileTexMap[file] = tex;
-        }
-
-        sf::Texture* tex = fileTexMap[file];
-
-        sf::Vector2u sz = tex->getSize();
-        xy size( static_cast<float>( sz.x ),
-                 static_cast<float>( sz.y ) );
-
-        return shared_ptr<Tex>( new TexImpl(tex, size) );
+        // TODO
+        return nullptr;
+//        if( fileTexMap.find(file) == fileTexMap.end() ) {
+//            // Load the texture
+//            sf::Image image;
+//            if( !image.loadFromFile(file) )
+//                throw invalid_argument("Unable to open file: " + file);
+//
+//            sf::Texture* tex = new sf::Texture;
+//            if( !tex->loadFromImage(image) )
+//                throw runtime_error("Loading image from texture " + file + " failed.");
+//
+//            fileTexMap[file] = tex;
+//        }
+//
+//        sf::Texture* tex = fileTexMap[file];
+//
+//        sf::Vector2u sz = tex->getSize();
+//        xy size( static_cast<float>( sz.x ),
+//                 static_cast<float>( sz.y ) );
+//
+//        return shared_ptr<Tex>( new TexImpl(tex, size) );
     }
 
     void drawImage(const Tex &tex, xy pos, bool horizontalFlip=false, float angle=0.0f) {
-        sf::Sprite sprite( *(dynamic_cast<const TexImpl&>(tex).tex) );
-        sprite.setPosition( pos.x + (horizontalFlip ? tex.getSize().x : 0) ,
-                            windowProperties.size.y - tex.getSize().y - pos.y );
-        sprite.setRotation(angle);
-        if(horizontalFlip) {
-            sprite.setScale(-1, 1);
-        }
-        renderWindow->draw(sprite);
+//        // TODO
+//        sf::Sprite sprite( *(dynamic_cast<const TexImpl&>(tex).tex) );
+//        sprite.setPosition( pos.x + (horizontalFlip ? tex.getSize().x : 0) ,
+//                            windowProperties.size.y - tex.getSize().y - pos.y );
+//        sprite.setRotation(angle);
+//        if(horizontalFlip) {
+//            sprite.setScale(-1, 1);
+//        }
+//        renderWindow->draw(sprite);
     }
 
     void drawText(string line, xy pos, Color color=Color(), float fontSize=15.0f) {
-        sf::Text textToDraw(line, defaultFont, static_cast<unsigned int>(fontSize));
-        textToDraw.setColor(sf::Color(color.r, color.g, color.b, color.a));
-        textToDraw.setPosition( static_cast<float>( pos.x ), static_cast<float>( pos.y ) );
-        renderWindow->draw(textToDraw);
+//        // TODO
+//        sf::Text textToDraw(line, defaultFont, static_cast<unsigned int>(fontSize));
+//        textToDraw.setColor(sf::Color(color.r, color.g, color.b, color.a));
+//        textToDraw.setPosition( static_cast<float>( pos.x ), static_cast<float>( pos.y ) );
+//        renderWindow->draw(textToDraw);
     }
 
     void drawBox(xy pos, xy size, Color fillColor, Color outlineColor, float outlineThickness) {
-        sf::RectangleShape rectangle;
-        rectangle.setPosition ( sf::Vector2f( pos.x,  windowProperties.size.y - pos.y) );
-        rectangle.setSize     ( sf::Vector2f( size.x, -size.y ) );
-        rectangle.setFillColor( sf::Color( fillColor.r, fillColor.g, fillColor.b, fillColor.a) );
-        rectangle.setOutlineThickness( outlineThickness );
-        rectangle.setOutlineColor( sf::Color(outlineColor.r, outlineColor.g, outlineColor.b, outlineColor.a) );
-        renderWindow->draw(rectangle);
+        cout<<"x";
+        SDL_Rect rect = {pos.x, pos.y, size.x, size.y};
+
+        SDL_SetRenderDrawColor(renderContext, outlineColor.r, outlineColor.g, outlineColor.b, outlineColor.a);
+        SDL_RenderDrawRect(renderContext, &rect);
+
+        SDL_SetRenderDrawColor(renderContext, fillColor.r, fillColor.g, fillColor.b, fillColor.a);
+        SDL_Rect inner_rect = getInnerRect(rect);
+        SDL_RenderFillRect(renderContext, &inner_rect);
+//        sf::RectangleShape rectangle;
+//        rectangle.setPosition ( sf::Vector2f( pos.x,  windowProperties.size.y - pos.y) );
+//        rectangle.setSize     ( sf::Vector2f( size.x, -size.y ) );
+//        rectangle.setFillColor( sf::Color( fillColor.r, fillColor.g, fillColor.b, fillColor.a) );
+//        rectangle.setOutlineThickness( outlineThickness );
+//        rectangle.setOutlineColor( sf::Color(outlineColor.r, outlineColor.g, outlineColor.b, outlineColor.a) );
+//        renderWindow->draw(rectangle);
     }
 
     void setMouseCursorVisibility(bool visibility) {
-        renderWindow->setMouseCursorVisible( visibility );
+//        TODO
+//        renderWindow->setMouseCursorVisible( visibility );
     }
 
     virtual void getInput(InputCallbacks *callbacks) {
-    	if( sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) )
-    		callbacks->escKey();
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W) )
-            callbacks->upKey();
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A) )
-            callbacks->leftKey();
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D) )
-            callbacks->rightKey();
-        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S) )
-            callbacks->downKey();
+//        TODO
+//    	if( sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) )
+//    		callbacks->escKey();
+//        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W) )
+//            callbacks->upKey();
+//        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A) )
+//            callbacks->leftKey();
+//        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D) )
+//            callbacks->rightKey();
+//        if( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S) )
+//            callbacks->downKey();
     }
 
     unsigned int random() {
@@ -95,7 +108,7 @@ public:
     }
 
     void exit() {
-        renderWindow->close();
+        isRunning = false;
     }
 
 private:
@@ -104,14 +117,14 @@ private:
 
     static SystemImpl* singleton;
 
-    map<const string, sf::Texture*> fileTexMap;
+    map<const string, SDL_Texture*> fileTexMap;
 
     class TexImpl : public Tex
     {
     public:
-        sf::Texture* tex;
+        SDL_Texture* tex;
 
-        TexImpl(sf::Texture* tex, xy size) :
+        TexImpl(SDL_Texture* tex, xy size) :
             tex(tex), size(size) {}
 
         const xy getSize() const {
@@ -123,9 +136,10 @@ private:
     };
 
     unique_ptr<GameMain> gameMain;
-    unique_ptr<sf::RenderWindow> renderWindow;
 
-    sf::Font defaultFont;
+    SDL_Window* renderWindow;
+    SDL_Renderer* renderContext;
+    bool isRunning;
 
     std::minstd_rand0 rng;
 
@@ -136,91 +150,129 @@ private:
                 windowProperties.fullscreen = true;
         }
         else if( argc == 4 && !strcmp(argv[1], "-r") ) {
-            windowProperties.size = xy( static_cast<float>( atoi(argv[2]) ),
-                                        static_cast<float>( atoi(argv[3]) ) );
+            windowProperties.size = xy(atoi(argv[2]), atoi(argv[3]) );
         }
     }
 
-    sf::RenderWindow* createRenderWindow()
+    void createWindowAndContext()
     {
         if(windowProperties.fullscreen) {
-            sf::VideoMode best_mode = sf::VideoMode::getFullscreenModes()[0];
-            windowProperties.size.x = static_cast<float>( best_mode.width );
-            windowProperties.size.y = static_cast<float>( best_mode.height );
-            sf::RenderWindow *renderWindow = new sf::RenderWindow(
-            		best_mode, windowProperties.title, sf::Style::Fullscreen);
-            renderWindow->setVerticalSyncEnabled(true);
-            return renderWindow;
+            // TODO
+            throw invalid_argument("Unimplemented");
+//            sf::VideoMode best_mode = sf::VideoMode::getFullscreenModes()[0];
+//            windowProperties.size.x = static_cast<float>( best_mode.width );
+//            windowProperties.size.y = static_cast<float>( best_mode.height );
+//            sf::RenderWindow *renderWindow = new sf::RenderWindow(
+//            		best_mode, windowProperties.title, sf::Style::Fullscreen);
+//            renderWindow->setVerticalSyncEnabled(true);
+//            return renderWindow;
         }
-        else
-            return new sf::RenderWindow(
-                sf::VideoMode( static_cast<int>( windowProperties.size.x ),
-                               static_cast<int>( windowProperties.size.y ), 32),
-                    windowProperties.title );
+        else {
+            renderWindow = SDL_CreateWindow(windowProperties.title.c_str(),
+                                160, 100, // TODO
+                                static_cast<int>( windowProperties.size.x ),
+                                static_cast<int>( windowProperties.size.y ),
+                                SDL_WINDOW_SHOWN);
+            if (renderWindow == nullptr){
+                throw invalid_argument(string("SDL_CreateWindow Error: ") + SDL_GetError());
+            }
+        }
+
+        SDL_Renderer* sdl_renderer = SDL_CreateRenderer(renderWindow, -1,
+                                         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (sdl_renderer == nullptr){
+            SDL_DestroyWindow(renderWindow);
+            throw invalid_argument(string("SDL_CreateRenderer Error: ") + SDL_GetError());
+        }
     }
 
-    string getFPS(int currentTime)
-    {
-        static const int updateFrequency = 10;
-        static int lastUpdate = 0,
-                lastFrameTime = 0,
-                duration = 0, fps = 0;
-        duration = currentTime - lastFrameTime;
-        if(duration && !lastUpdate) {
-            fps = 1000 / duration;
-            lastUpdate = updateFrequency;
-        }
-        lastFrameTime = currentTime;
-        lastUpdate--;
+//    string getFPS(int currentTime)
+//    {
+//        static const int updateFrequency = 10;
+//        static int lastUpdate = 0,
+//                lastFrameTime = 0,
+//                duration = 0, fps = 0;
+//        duration = currentTime - lastFrameTime;
+//        if(duration && !lastUpdate) {
+//            fps = 1000 / duration;
+//            lastUpdate = updateFrequency;
+//        }
+//        lastFrameTime = currentTime;
+//        lastUpdate--;
+//
+//        stringstream s;
+//        s<<fps;
+//        return s.str();
+//    }
 
-        stringstream s;
-        s<<fps;
-        return s.str();
+    static SDL_Rect getInnerRect(const SDL_Rect &rect) {
+        SDL_Rect innerRect;
+        innerRect.x = rect.x + 1;
+        innerRect.y = rect.y + 1;
+        innerRect.w = rect.w - 2;
+        innerRect.h = rect.h - 2;
+        return innerRect;
     }
 
-    void main(int argc, char *argv[])
+    int platformMain(int argc, char *argv[])
     {
         windowProperties = GameMain::defaultWindowProperties();
 
         handleCmdlineArgs(argc, argv);
 
-        renderWindow = unique_ptr<sf::RenderWindow>( createRenderWindow() );
-        renderWindow->clear();
-        renderWindow->setFramerateLimit( 60 );
+        /*
+         * Platform Setup
+         */
+        try {
+            createWindowAndContext();
+        }
+        catch(exception &e) {
+            cerr<<"Exception caught: "<<e.what()<<endl;
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
 
-        // setup the default Font
-        defaultFont.loadFromFile("assets/fonts/opensans-regular-webfont.ttf");
-
+        /*
+         * Game Loop
+         */
         try
         {
             gameMain = unique_ptr<GameMain>( GameMain::getSingleton() );
+            isRunning = true;
 
-            sf::Clock clock;
-
-            while( renderWindow->isOpen() )
+            while(isRunning)
             {
-                sf::Event event;
-                while( renderWindow->pollEvent(event) )
+                SDL_Event sdl_event;
+                while( SDL_PollEvent(&sdl_event) )
                 {
-                    if( event.type == sf::Event::Closed )
-                        renderWindow->close();
+                    if( sdl_event.type == SDL_QUIT ) {
+                        isRunning = false; break;
+                    }
+                    else if (sdl_event.type == SDL_KEYDOWN && sdl_event.key.keysym.sym == SDLK_ESCAPE) {
+                        isRunning = false; break;
+                    }
                 }
 
-                renderWindow->clear();
+                SDL_RenderClear(renderContext);
 
                 gameMain->step();
 
-                Sys()->drawText(getFPS( clock.getElapsedTime().asMilliseconds() ),
-                        xy(Sys()->getWindowProperties().size.x - 30, 10));
-
-                renderWindow->display();
+                SDL_RenderPresent(renderContext);
             }
+
+            SDL_DestroyRenderer(renderContext);
+            SDL_DestroyWindow(renderWindow);
+            SDL_Quit();
+            return EXIT_SUCCESS;
         }
         catch(exception &e)
         {
             cerr<<"Exception caught: "<<e.what()<<endl;
-            renderWindow->close();
-            cerr<<"Terminating..."<<endl;
+
+            SDL_DestroyRenderer(renderContext);
+            SDL_DestroyWindow(renderWindow);
+            SDL_Quit();
+            return EXIT_FAILURE;
         }
     }
 };
@@ -233,7 +285,7 @@ System* Sys() {
 
 int main(int argc, char *argv[]) {
     SystemImpl *sys = SystemImpl::singleton = new SystemImpl();
-    sys->main(argc, argv);
+    int retCode = sys->platformMain(argc, argv);
     delete SystemImpl::singleton;
-    return 0;
+    return retCode;
 }
